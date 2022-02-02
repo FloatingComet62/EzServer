e49c918322049c1995fae9e040c7249d7b0003c5
FloatingComet62 <shubh2007rai@gmail.com>
2022-02-02 13:23:48 +0530

    forgot the add middleware in the page.py comment

--- src/pages.py
+++ src/pages.py
@@ -2,7 +2,7 @@ import callbackfunctions
 import middleware
 
 pages = [
-    # [PATH, HTML_FILE, CALLBACK_FUNCTION.__name__]
+    # [PATH, HTML_FILE, MIDDLEWARE_FUNCTION.__name__ CALLBACK_FUNCTION.__name__]
     ["/", ['src\pages\home.html', 'src\pages\failed.html'], "", middleware.homePage.__name__],
     ["/exit", ['src\pages\exit.html', 'src\pages\failed.html'], callbackfunctions.exitPage.__name__, middleware.exitPage.__name__],
 ]
