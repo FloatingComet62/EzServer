42a1cd5040dd09c910bc388b1344c053f7a2fed1
FloatingComet62 <shubh2007rai@gmail.com>
2022-02-02 14:21:46 +0530

    Added a cool default home page

--- src/pages.py
+++ src/pages.py
@@ -3,6 +3,7 @@ import middleware
 
 pages = [
     # [PATH, HTML_FILE, CALLBACK_FUNCTION.__name__, MIDDLEWARE_FUNCTION.__name__]
-    ["/", ['src\pages\home.html', 'src\pages\failed.html'], "", middleware.homePage.__name__],
-    ["/exit", ['src\pages\exit.html', 'src\pages\failed.html'], callbackfunctions.exitPage.__name__, middleware.exitPage.__name__],
+    ["/", ["src\pages\main.html"]],
+    ["/home", ["src\pages\home.html", "src\pages\\failed.html"], "", middleware.homePage.__name__],
+    ["/exit", ["src\pages\exit.html", "src\pages\\failed.html"], callbackfunctions.exitPage.__name__, middleware.exitPage.__name__],
 ]
