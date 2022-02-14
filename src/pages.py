f7b2a031c5dcf2eea658eb89dc7c48f63e34bbfb
FloatingComet62 <shubh2007rai@gmail.com>
2022-02-14 23:21:57 +0530

    Forgot to add the file type on other pages

--- src/pages.py
+++ src/pages.py
@@ -4,6 +4,6 @@ import middleware
 pages = [
     # [PATH, FILE_TYPE, HTML_FILE, CALLBACK_FUNCTION.__name__, MIDDLEWARE_FUNCTION.__name__]
     ["/", "html", ["src\pages\main.html"]],
-    ["/home", ["src\pages\home.html", "src\pages\\failed.html"], "", middleware.homePage.__name__],
-    ["/exit", ["src\pages\exit.html", "src\pages\\failed.html"], callbackfunctions.exitPage.__name__, middleware.exitPage.__name__],
+    ["/home", "html", ["src\pages\home.html", "src\pages\\failed.html"], "", middleware.homePage.__name__],
+    ["/exit", "html", ["src\pages\exit.html", "src\pages\\failed.html"], callbackfunctions.exitPage.__name__, middleware.exitPage.__name__],
 ]
